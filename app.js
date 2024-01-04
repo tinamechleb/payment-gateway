d4d9afc08e729b43523f4b89691b1e9c0807c294
Tina Mechleb <tina.mechleb@menasa.net>
2024-01-04 11:39:51 +0200

    added UI

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const paymentRoutes = require('./server/routes/paymentRoutes');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use('/api/payment', paymentRoutes);
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
